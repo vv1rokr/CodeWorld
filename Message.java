1c7c988ecc3a77e55c3d044a221c0ba4f703c575
Vivek <128774789+vv1rokr@users.noreply.github.com>
2023-05-05 13:25:12 +0530

    First code added

---
+++ Message.java
@@ -0,0 +1,19 @@
+//package name= folder name
+package day1;
+//in java everything is written inside class
+//class - keyword
+//classname = Filename 
+// filename should begin with capital letter 
+// comments --> //
+
+//wap to print hello message
+public class Message {
+
+	public static void main(String[] args) {
+		
+       System.out.println("hello");
+       System.out.println("welcome");
+		
+	}
+
+}
